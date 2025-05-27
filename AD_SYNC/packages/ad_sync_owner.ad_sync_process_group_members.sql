e2877123a9158c00cfed6515be81d95e519719ba
Mariusz Masewicz <orasup003@eos-ksi.pl>
2025-05-27 12:17:18 +0200

    delete group member

--- AD_SYNC/packages/ad_sync_owner.ad_sync_process_group_members.sql
+++ AD_SYNC/packages/ad_sync_owner.ad_sync_process_group_members.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE PACKAGE AD_SYNC_OWNER.AD_SYNC_PROCESS_GROUP_MEMBERS AUTHID CUR
    ,P_LOAD_ID         NUMBER
   );
     --PROCEDURE drop_group_members_not_exist_in_load (p_start_timestamp timestamp, p_end_timestamp timestamp, p_process_run number, p_load_id number);
-  PROCEDURE DROP_GROUP_MEMBER_ON_DEMAND (
+  PROCEDURE DROP_GROUP_MEMBERS_ON_DEMAND (
     P_START_TIMESTAMP TIMESTAMP
    ,P_END_TIMESTAMP   TIMESTAMP
    ,P_PROCESS_RUN     NUMBER
@@ -79,80 +79,66 @@ CREATE OR REPLACE PACKAGE BODY AD_SYNC_OWNER.AD_SYNC_PROCESS_GROUP_MEMBERS AS
     END drop_group_members_not_exist_in_load;
 */
 
-  PROCEDURE DROP_GROUP_MEMBER_ON_DEMAND (
-    P_START_TIMESTAMP TIMESTAMP
-   ,P_END_TIMESTAMP   TIMESTAMP
-   ,P_PROCESS_RUN     NUMBER
-   ,P_LOAD_ID         NUMBER
-  ) AS
-    V_STMT VARCHAR2(4000);
-  BEGIN
-    IF P_PROCESS_RUN IS NOT NULL THEN
-      AD_SYNC_LOG.WRITE_INFO(
-        $$PLSQL_UNIT
-        || '->drop_gropus started for: '
-        || P_START_TIMESTAMP
-        || ' and '
-        || P_END_TIMESTAMP
-       ,SQLCODE
-       ,SQLERRM
-       ,P_PROCESS_RUN
-      );
-      FOR I IN (
-        SELECT AG.ID
-              ,AG.GROUPNAME
-              ,AG.MEMBER
-          FROM AD_SYNC_OWNER.AD_SYNC_GROUP_MEMBERS AG
-          JOIN AD_SYNC_OWNER.AD_SYNC_MANAGED_GROUP_MEMBERS G
-        ON ( AG.GROUPNAME = G.GROUPNAME
-           AND AG.MEMBER = G.GROUP_MEMBER )
-         WHERE AG.STATUS = 1
-           AND AG.REQUESTED_OPERATION = 'D' --requested drop GROUP
-        AND AG.GROUPNAME LIKE AD_SYNC_OWNER.AD_SYNC_TOOLS.GET_PARAM_VALUE('GROUPNAME_PREFIX')
-               || '%'
-        AND AG.MEMBER LIKE AD_SYNC_OWNER.AD_SYNC_TOOLS.GET_PARAM_VALUE('USERNAME_PREFIX')
-                           || '%'
-           AND AG.CREATED_TIMESTAMP BETWEEN P_START_TIMESTAMP AND P_END_TIMESTAMP
-      ) LOOP
-        V_STMT := 'revoke '
-                  || I.GROUPNAME
-                  || ' from '
-                  || I.MEMBER;
-        DBMS_OUTPUT.PUT_LINE(V_STMT || ';');
-        AD_SYNC_LOG.WRITE_INFO(
-          $$PLSQL_UNIT
-          || '->drop_gropus group dropped: '
-          || I.GROUPNAME
-         ,SQLCODE
-         ,SQLERRM
-         ,P_PROCESS_RUN
-        );
-        EXECUTE IMMEDIATE V_STMT;
-        UPDATE AD_SYNC_OWNER.AD_SYNC_GROUP_MEMBERS
-           SET STATUS = 32 -- GROUP dropped
-           ,PROCESS_TIMESTAMP = CURRENT_TIMESTAMP
-        ,LOAD_ID = P_LOAD_ID
-         WHERE ID = I.ID;
+PROCEDURE drop_group_members_on_demand (p_start_timestamp timestamp, p_end_timestamp timestamp, p_process_run number, p_load_id number) as
+        v_stmt VARCHAR2(4000);
+    BEGIN
+        if p_process_run is not null then
+        ad_sync_log.write_info($$PLSQL_UNIT || 
+                            '->drop_group_members_on_demand started for: '||p_start_timestamp||' and '||p_end_timestamp,
+                            SQLCODE,
+                            SQLERRM,
+                            p_process_run);
+        FOR i IN (
+            SELECT
+                ag.id,ag.groupname, ag.member
+            FROM
+                ad_sync_owner.ad_sync_group_members ag 
+            WHERE
+                ag.status = 1
+                and ag.REQUESTED_OPERATION = 'D' --requested drop member from group
+                and ag.groupname like ad_sync_owner.ad_sync_tools.get_param_value('GROUPNAME_PREFIX')||'%'
+                and ag.member like ad_sync_owner.ad_sync_tools.get_param_value('USERNAME_PREFIX')||'%'
+                and ag.CREATED_TIMESTAMP between p_start_timestamp and p_end_timestamp
+                and ag.groupname is not null
+                and ag.member is not null
+                )
+         LOOP
+            v_stmt :=  'REVOKE '||i.groupname||' from '||i.member
+                   ;
+            dbms_output.put_line(v_stmt||';');
 
-      END LOOP;
+            ad_sync_log.write_info($$PLSQL_UNIT || 
+                            '->drop_group_members_on_demand add member to group: '|| i.member||':'||i.groupname,
+                            SQLCODE,
+                            SQLERRM,
+                            p_process_run);
+            execute immediate v_stmt;
+            UPDATE ad_sync_owner.AD_SYNC_group_members
+            SET
+                status = 42 -- GROUP_member dropped from group
+                , PROCESS_TIMESTAMP = current_timestamp
+                , load_id=p_load_id
+            WHERE
+                id = i.id;
 
-      COMMIT;
-      AD_SYNC_LOG.WRITE_INFO(
-        $$PLSQL_UNIT || '->drop_gropus  finished'
-       ,SQLCODE
-       ,SQLERRM
-       ,P_PROCESS_RUN
-      );
-    END IF;
-  EXCEPTION
-    WHEN OTHERS THEN
-      AD_SYNC_LOG.WRITE_ERROR(
-        $$PLSQL_UNIT || '->drop_gropus'
-       ,SQLCODE
-       ,SQLERRM
-      );
-      RAISE;
-  END DROP_GROUP_MEMBER_ON_DEMAND;
+        END LOOP;
+
+        COMMIT;
+        ad_sync_log.write_info($$PLSQL_UNIT || 
+                            '->drop_group_members_on_demand  finished',
+                            SQLCODE,
+                            SQLERRM,
+                            p_process_run);
+         end if;                   
+    EXCEPTION
+      WHEN OTHERS THEN
+        ad_sync_log.write_error($$PLSQL_UNIT ||
+                            '->drop_group_members_on_demand' ,
+                            SQLCODE,
+                            SQLERRM);
+        RAISE;
+
+    END drop_group_members_on_demand;
 
   PROCEDURE MARK_EXISTING_GROUP_MEMBERS (
     P_START_TIMESTAMP TIMESTAMP
