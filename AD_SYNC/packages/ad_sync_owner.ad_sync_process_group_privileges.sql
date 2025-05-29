503e75294c0e43a5e7e9adc6952973877641c641
Mariusz Masewicz <Mariusz.Masewicz@summ-it.pl>
2025-05-29 17:08:54 +0200

    group_privileges management

--- AD_SYNC/packages/ad_sync_owner.ad_sync_process_group_privileges.sql
+++ AD_SYNC/packages/ad_sync_owner.ad_sync_process_group_privileges.sql
@@ -8,12 +8,184 @@ CREATE OR REPLACE PACKAGE AD_SYNC_OWNER.AD_SYNC_PROCESS_GROUP_PRIVILEGES AUTHID 
   PROCEDURE ADD_OBJECT_PRIVILEGES (
     P_PROCESS_RUN NUMBER
   );
+
+  PROCEDURE clean_group (
+    p_group VARCHAR2(1000),
+    P_PROCESS_RUN NUMBER
+  );
+
+  PROCEDURE clean_all_groups (
+    p_group VARCHAR2(1000),
+    P_PROCESS_RUN NUMBER
+  );
+
+ PROCEDURE grant_privileges_to_groups (
+    P_PROCESS_RUN NUMBER
+  );
+
 END AD_SYNC_PROCESS_GROUP_PRIVILEGES;
 /
 
 prompt CREATE OR REPLACE PACKAGE BODY ad_sync_owner.ad_sync_process_group_privileges
 
 CREATE OR REPLACE PACKAGE BODY AD_SYNC_OWNER.AD_SYNC_PROCESS_GROUP_PRIVILEGES AS
+
+ PROCEDURE grant_privileges_to_groups (
+    P_PROCESS_RUN NUMBER
+  ) is
+  begin
+    AD_SYNC_LOG.WRITE_INFO(
+      $$PLSQL_UNIT || '->grant_privileges_to_groups started'
+     ,SQLCODE
+     ,SQLERRM
+     ,P_PROCESS_RUN
+    );
+    --clean all groups
+    --clean_all_groups('*', P_PROCESS_RUN);
+    --add system privileges and roles
+    ADD_SYSTEM_PRIVILEGES_AND_ROLES(P_PROCESS_RUN);
+    --add object privileges
+    ADD_OBJECT_PRIVILEGES(P_PROCESS_RUN);
+
+    AD_SYNC_LOG.WRITE_INFO(
+      $$PLSQL_UNIT || '->grant_privileges_to_groups finished'
+     ,SQLCODE
+     ,SQLERRM
+     ,P_PROCESS_RUN
+    );
+  EXCEPTION
+    WHEN OTHERS THEN
+      AD_SYNC_LOG.WRITE_ERROR(
+        $$PLSQL_UNIT || '->grant_privileges_to_groups'
+       ,SQLCODE
+       ,SQLERRM
+       ,P_PROCESS_RUN
+      );
+      RAISE;
+      end grant_privileges_to_groups;
+
+
+
+ PROCEDURE clean_all_groups (
+    p_group VARCHAR2(1000),
+    P_PROCESS_RUN NUMBER
+  ) is
+  begin
+    AD_SYNC_LOG.WRITE_INFO(
+      $$PLSQL_UNIT || '->clean_all_groups started'
+     ,SQLCODE
+     ,SQLERRM
+     ,P_PROCESS_RUN
+    );
+    --clean all groups
+    FOR I IN (
+      SELECT role AS GROUPNAME
+        FROM dba_roles
+       WHERE role LIKE AD_SYNC_OWNER.AD_SYNC_TOOLS.GET_PARAM_VALUE('GROUPNAME_PREFIX')
+    ) LOOP
+      clean_group(I.GROUPNAME, P_PROCESS_RUN);
+    END LOOP;
+
+    AD_SYNC_LOG.WRITE_INFO(
+      $$PLSQL_UNIT || '->clean_all_groups finished'
+     ,SQLCODE
+     ,SQLERRM
+     ,P_PROCESS_RUN
+    );
+  end clean_all_groups;
+
+  PROCEDURE clean_group (
+    p_group (1000),
+    P_PROCESS_RUN NUMBER
+  ) is
+  BEGIN
+    AD_SYNC_LOG.WRITE_INFO(
+      $$PLSQL_UNIT || '->clean_group started'
+     ,SQLCODE
+     ,SQLERRM
+     ,P_PROCESS_RUN
+    );
+    --clean roles granted to role
+    FOR I IN (
+      SELECT 'revoke '
+             || granted_role
+             || ' from '
+             || role AS STMT
+        FROM role_role_privs
+       WHERE role = p_group
+       and role LIKE AD_SYNC_OWNER.AD_SYNC_TOOLS.GET_PARAM_VALUE('GROUPNAME_PREFIX')
+    ) LOOP
+      BEGIN
+        --DBMS_OUTPUT.PUT_LINE(I.STMT);
+        EXECUTE IMMEDIATE I.STMT;
+      EXCEPTION
+        WHEN OTHERS THEN
+          AD_SYNC_LOG.WRITE_ERROR(
+            $$PLSQL_UNIT || '->clean_group'
+           ,SQLCODE
+           ,SQLERRM
+           ,P_PROCESS_RUN
+          );
+      END;
+    END LOOP;
+    --clean system privileges granted to role
+    FOR I IN (
+      SELECT 'revoke '
+             || privilege
+             || ' from '
+             || role AS STMT
+        FROM role_sys_privs
+       WHERE role = p_group
+       and role LIKE AD_SYNC_OWNER.AD_SYNC_TOOLS.GET_PARAM_VALUE('GROUPNAME_PREFIX')
+    ) LOOP
+      BEGIN
+        --DBMS_OUTPUT.PUT_LINE(I.STMT);
+        EXECUTE IMMEDIATE I.STMT;
+      EXCEPTION
+        WHEN OTHERS THEN
+          AD_SYNC_LOG.WRITE_ERROR(
+            $$PLSQL_UNIT || '->clean_group'
+           ,SQLCODE
+           ,SQLERRM
+           ,P_PROCESS_RUN
+          );
+      END;
+    END LOOP;
+    --clean object privileges granted to role
+    FOR I IN (
+      SELECT 'revoke '
+             || privilege
+             || ' on '
+             || owner
+             || '.'
+             || table_name
+             || ' from '
+             || role AS STMT
+        FROM role_sys_privs
+       WHERE role = p_group
+       and role LIKE AD_SYNC_OWNER.AD_SYNC_TOOLS.GET_PARAM_VALUE('GROUPNAME_PREFIX')
+    ) LOOP
+      BEGIN
+        --DBMS_OUTPUT.PUT_LINE(I.STMT);
+        EXECUTE IMMEDIATE I.STMT;
+      EXCEPTION
+        WHEN OTHERS THEN
+          AD_SYNC_LOG.WRITE_ERROR(
+            $$PLSQL_UNIT || '->clean_group'
+           ,SQLCODE
+           ,SQLERRM
+           ,P_PROCESS_RUN
+          );
+      END;
+    END LOOP;
+    AD_SYNC_LOG.WRITE_INFO(
+      $$PLSQL_UNIT || '->clean_group finished'
+     ,SQLCODE
+     ,SQLERRM
+     ,P_PROCESS_RUN
+    );
+  end clean_group;
+
 
   PROCEDURE ADD_SYSTEM_PRIVILEGES_AND_ROLES (
     P_PROCESS_RUN NUMBER
